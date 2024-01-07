dd51682df25601da9130faab80a42660fcd5d03f
Azharr110 <abdazhar786@gmail.com>
2024-01-07 23:20:41 +0530

    Add questions

---
+++ script.js
@@ -0,0 +1,38 @@
+const question = [
+    {
+        question: "Which is largest animal in the world",
+        answers: [
+            {test: "Shark", correct: false},
+            {test: "Blue Whale", correct: true},
+            {test: "Elephant", correct: false},
+            {test: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world",
+        answers: [
+            {test: "Vatican City", correct: true},
+            {test: "Bhutan", correct: false},
+            {test: "Behrain", correct: false},
+            {test: "Oman", correct: false},
+        ]
+    },
+    {
+        question: "Which Batsman has scored the Fastest 100 in ODI",
+        answers: [
+            {test: "Misbah ul haq", correct: false},
+            {test: "David Miller", correct: true},
+            {test: "Ab de Villiers", correct: false},
+            {test: "Rohit Sharma", correct: true},
+        ]
+    },
+    {
+        question: "Which Batsman has scored the Fastest 100 in T-20",
+        answers: [
+            {test: "Misbah ul haq", correct: false},
+            {test: "David Miller", correct: true},
+            {test: "Ab de Villiers", correct: false},
+            {test: "Rohit Sharma", correct: false},
+        ]
+    },
+]
