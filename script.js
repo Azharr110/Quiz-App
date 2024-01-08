fdaf3fc80351c642dbff99c8d99618f3b763ba45
Azharr110 <abdazhar786@gmail.com>
2024-01-08 17:42:58 +0530

    Add startQuiz and showQuestion

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const question = [
+const questions = [
     {
         question: "Which is largest animal in the world",
         answers: [
@@ -36,3 +36,32 @@ const question = [
         ]
     },
 ]
+
+const questionElement =  document.getElementById("question")
+const answerButton =  document.getElementById("answer-buttons")
+const nextButton =  document.getElementById("next-btn")
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerHTML = answer.text;
+        button.classList.add("btn")
+        answerButton.appendChild(button);
+
+    })
+} 
+startQuiz();
