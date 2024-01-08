205c0435d50c161c325f2d9e4ba6d3682709c842
Azharr110 <abdazhar786@gmail.com>
2024-01-08 22:15:09 +0530

    Add reset and  select answer

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const questions = [
 ]
 
 const questionElement =  document.getElementById("question")
-const answerButton =  document.getElementById("answer-buttons")
+const answerButtons =  document.getElementById("answer-buttons")
 const nextButton =  document.getElementById("next-btn")
 
 let currentQuestionIndex = 0;
@@ -52,6 +52,7 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -60,8 +61,29 @@ function showQuestion(){
         const button = document.createElement("button")
         button.innerHTML = answer.text;
         button.classList.add("btn")
-        answerButton.appendChild(button);
-
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct =answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
     })
 } 
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+}
+
 startQuiz();
